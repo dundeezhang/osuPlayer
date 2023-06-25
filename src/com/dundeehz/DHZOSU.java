ac17e181dbdf40f6ddc02d73486c28fd1a0ca3bd
Dundee <dundee_zhang@hotmail.com>
2023-06-25 13:47:21 -0400

    added select osu location

---
+++ src/com/dundeehz/DHZOSU.java
@@ -0,0 +1,136 @@
+package com.dundeehz;
+
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class DHZOSU {
+    private JPanel panelArea;
+    private JTextField searchBox;
+    private JLabel searchLabel;
+    private JTextArea logArea;
+    private JLabel previousSongsLabel;
+    private JCheckBox shuffleCheckBox;
+    private JSlider timerSlider;
+    private JLabel optionsLabel;
+    private JLabel sleepTimerSliderLabel;
+    private JCheckBox loopCurrentSongCheckBox;
+    private JButton playRandomButton;
+    private JLabel currentCollectionLabel;
+    private JLabel collectionsLabel;
+    private JLabel currentCollectionDisplay;
+    private JButton selectCollectionButton;
+    private JButton quitButton;
+    private JButton selectOsuFolderButton;
+    private final File data;
+
+    public DHZOSU() {
+
+        data = new File("data.txt");
+        quitButton.addActionListener(e -> System.exit(0));
+
+        // search for a song to play next
+        searchBox.addActionListener(e -> {
+            try {
+                search(searchBox);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        selectOsuFolderButton.addActionListener(e -> {
+            selectFileLocation();
+            Songs songs = new Songs(data);
+            try {
+                songs.initSongs();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+
+    private void selectFileLocation() {
+        try {
+            String osuPath = Objects.requireNonNull(getInputPath(null)).toString();
+            BufferedWriter bufferWrite = null;
+            try {
+
+                /* This logic will make sure that the file
+                 * gets created if it is not present at the
+                 * specified location*/
+                if (!data.exists()) {
+                    data.createNewFile();
+                }
+
+                FileWriter fileWrite = new FileWriter(data, false);
+                bufferWrite = new BufferedWriter(fileWrite);
+                bufferWrite.write(osuPath + "\n");
+                logArea.setText(logArea.getText() + "OSU.EXE SELECTED\n");
+
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } finally {
+                try {
+                    if (bufferWrite != null)
+                        bufferWrite.close();
+                } catch (Exception ex) {
+                    System.out.println("Error in closing the BufferedWriter" + ex);
+                }
+            }
+        } catch (Exception e2) {
+            logArea.setText(logArea.getText() + "No file was returned\n");
+        }
+    }
+
+    private void search(JTextField field) throws IOException {
+        ArrayList<String> songInfo = new ArrayList<>();
+        if (checkPath()) {
+            String search = field.getText();
+            BufferedReader read = new BufferedReader(new FileReader(data));
+            String lineRead;
+            while ((lineRead = read.readLine()) != null) {
+                songInfo.add(lineRead);
+            }
+
+        }
+
+    }
+
+    // https://stackoverflow.com/questions/51973636/how-to-return-the-file-path-from-the-windows-file-explorer-using-java
+    public static Path getInputPath(String s) { // code stolen from stackoverflow
+         /*Send a path (a String path) to open in a specific directory
+         or if null default directory */
+        JFileChooser jd = s == null ? new JFileChooser() : new JFileChooser(s);
+
+        jd.setDialogTitle("Choose input file");
+        int returnVal = jd.showOpenDialog(null);
+
+        /* If user didn't select a file and click ok, return null Path object*/
+        if (returnVal != JFileChooser.APPROVE_OPTION) return null;
+        return jd.getSelectedFile().toPath();
+    }
+
+    /**
+     * Main Method
+     *
+     * @param args Command Line Arguments
+     */
+    public static void main(String[] args) {
+        JFrame window = new JFrame("DHZ Osu Player");
+        window.setContentPane(new DHZOSU().panelArea);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+
+    private boolean checkPath() throws IOException {
+        BufferedReader test = new BufferedReader(new FileReader(data));
+        if (Objects.equals(test.readLine(), "-1")) {
+            logArea.setText(logArea.getText() + "PLEASE SELECT OSU.EXE\n");
+            return false;
+        }
+        return true;
+    }
+}
